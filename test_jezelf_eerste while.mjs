c93dcdb6ced23bc13311dff091d373d5424b6e91
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-04 09:46:23 +0200

    update

--- test_jezelf_eerste while.mjs
+++ test_jezelf_eerste while.mjs
@@ -3,10 +3,11 @@ import{stdin as input, stdout as output} from 'node:process';
 import { parse } from 'node:path';
 const userInput = readline.createInterface({input, output});
 
-let countdown = 100;
+let i = 0;
 
-while(countdown >= 0){
-   console.log(countdown--);
+while (i <= 12) {
+    console.log(i);
+    i += 3;
 }
 
 process.exit();
